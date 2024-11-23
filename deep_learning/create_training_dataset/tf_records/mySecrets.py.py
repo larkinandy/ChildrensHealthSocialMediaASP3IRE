6c323d82c8b5cbfe83a07a7aed89064d1727b859
Andrew Larkin <larkinan@oregonstate.edu>
2024-11-23 11:35:25 -0800

    Delete mySecrets.py.py

--- deep_learning/create_training_dataset/tf_records/mySecrets.py.py
+++
@@ -1,7 +0,0 @@
-PARENT_FOLDER = "/mnt/e/Aspire/BERT/"
-secrets = {
-    'PARENT_FOLDER':PARENT_FOLDER,
-    'TEXT_FOLDER':PARENT_FOLDER + "BERT_text/",
-    'TF_RECORDS_FOLDER':PARENT_FOLDER + "tfRecords/",
-    'TokenPath':PARENT_FOLDER + 'expandedTokenBase'
-}
