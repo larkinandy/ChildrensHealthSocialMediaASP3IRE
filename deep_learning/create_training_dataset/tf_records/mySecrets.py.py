aa698904a80261ebcd0a82800ab08120ff390d83
Andrew Larkin <larkinan@oregonstate.edu>
2024-11-23 11:33:26 -0800

    Create mySecrets.py.py

---
+++ deep_learning/create_training_dataset/tf_records/mySecrets.py.py
@@ -0,0 +1,7 @@
+PARENT_FOLDER = "/mnt/e/Aspire/BERT/"
+secrets = {
+    'PARENT_FOLDER':PARENT_FOLDER,
+    'TEXT_FOLDER':PARENT_FOLDER + "BERT_text/",
+    'TF_RECORDS_FOLDER':PARENT_FOLDER + "tfRecords/",
+    'TokenPath':PARENT_FOLDER + 'expandedTokenBase'
+}
