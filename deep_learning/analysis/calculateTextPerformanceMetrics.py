51f19cf442a1c445a8c5110cd171e898d776e426
Andrew Larkin <larkinan@oregonstate.edu>
2024-11-27 13:01:25 -0800

    added comments

--- deep_learning/analysis/calculateTextPerformanceMetrics.py
+++ deep_learning/analysis/calculateTextPerformanceMetrics.py
@@ -1,13 +1,33 @@
+# calculateTextPerformanceMetrics.py #
+# Author: Andrew Larkin
+# Date Created: November 27th, 2024
+
+# Summary: given validation datasets, calculate a confusion matrix for one of the 
+#          Aspire social media text deep learning models 
+
+# import libraries
 import pandas as ps
 import numpy as np
 import matplotlib.pyplot as plt
 import tensorflow as tf
 import transformers
 
-# custom classes
+# import custom classes
 from TFRecordRead_Class import TFRecordRead
 from mySecrets import secrets
 
+
+# global constants
+AUTOTUNE = tf.data.experimental.AUTOTUNE
+TRAIN_BATCH_SIZE = 128
+VAL_BATCH_SIZE = 128
+VALIDATION_SIZE = 5000 # number of records in validation dataset
+
+# use custom class to load TF records 
+# INPUTS:
+#    dataFolder (str) - folderpath where tf records are stored
+# OUTPUTS:
+#    dataset (tf record reader)
 def loadDataset(dataFolder):
     recordReader = TFRecordRead(dataFolder,128,2)
     dataset = recordReader.tfRecords.prefetch(buffer_size=AUTOTUNE)
@@ -15,26 +35,16 @@ def loadDataset(dataFolder):
     print("loaded dataset")
     return(dataset)
 
-print("Num GPUs Available: ", len(tf.config.list_physical_devices('GPU')))
-print(tf.config.list_physical_devices('GPU'))
-AUTOTUNE = tf.data.experimental.AUTOTUNE
-TRAIN_BATCH_SIZE = 128
-VAL_BATCH_SIZE = 128
-testData = loadDataset(secrets['TEST_DATA_FOLDER'])
-
-def getLabels():
-    labelSet = []
-    for images, labels in testData.take(1000):
-        if(len(labelSet)==0):
-            labelSet = labels.numpy()
-        else:
-            labelSet = np.concatenate((labelSet,labels))
-    print(labelSet.shape)
-    return(labelSet)
-
+# predict outputs for validation dataset
+# INPUTS:
+#    dataset (tf record reader) - object that will serve tf records to the model
+#    model (tf model) - model that will generate predictions
+# OUTPUTS:
+#    labelSet (list of np int arrays) - multilabel classifications for validation dataset
+#    predictions (list of np float arrays) - multilabel probabilities predicted by model
 def getPredictions(dataset,model):
     labelSet,predictions = [],[]
-    for text, labels in testData.take(1000):
+    for text, labels in testData.take(VALIDATION_SIZE):
         print(text)
         pred = model.predict(text)
         if(len(labelSet)==0):
@@ -45,7 +55,13 @@ def getPredictions(dataset,model):
             predictions = np.concatenate((predictions,pred))
     return([labelSet,predictions])
 
-
+# calculate the number of true positive examples predicted by the model
+# INPUTS:
+#    labels (list of np int arrays) - multilabel classifications
+#    predictions (list of np int arrays) - multilabel predictions
+#    debug (boolean) - whether to execute branching debug commands
+# OUTPUTS:
+#    nCorrect (int) - number of true positive predictions
 def calcTruePositive(labels,predictions,debug=False):
     correct = np.multiply(labels,predictions)
     nCorrect = np.sum(correct,axis=0)
@@ -54,6 +70,13 @@ def calcTruePositive(labels,predictions,debug=False):
         return([nCorrect,percCorrect])
     return(nCorrect)
 
+# calculate the number of true negative examples predicted by the model
+# INPUTS:
+#    labels (list of np int arrays) - multilabel classifications
+#    predictions (list of np int arrays) - multilabel predictions
+#    debug (boolean) - whether to execute branching debug commands
+# OUTPUTS:
+#    nCorrect (int) - number of true negative predictions
 def calcTrueNegative(labels,predictions,debug=False):
     labZeros = np.where(labels==0,1,0)
     predZeros = np.where(predictions==0,1,0)
@@ -64,24 +87,44 @@ def calcTrueNegative(labels,predictions,debug=False):
         return([nCorrect,percCorrect])
     return(nCorrect)
 
+# calculate the number of false negative examples predicted by the model
+# INPUTS:
+#    labels (list of np int arrays) - multilabel classifications
+#    predictions (list of np int arrays) - multilabel predictions
+#    debug (boolean) - whether to execute branching debug commands
+# OUTPUTS:
+#    nIncorrect (int) - number of false negative predictions
 def calcFalseNegative(labels,predictions,debug=False):
     predZeros = np.where(predictions==0,1,0)
     correct = np.multiply(labels,predZeros)
-    nCorrect = np.sum(correct,axis=0)
+    nIncorrect = np.sum(correct,axis=0)
     if debug:
-        percCorrect = nCorrect/np.sum(labels,axis=0)*100
-        return([nCorrect,percCorrect])
-    return(nCorrect)
+        percCorrect = nIncorrect/np.sum(labels,axis=0)*100
+        return([nIncorrect,percCorrect])
+    return(nIncorrect)
 
+# calculate the number of false positive examples predicted by the model
+# INPUTS:
+#    labels (list of np int arrays) - multilabel classifications
+#    predictions (list of np int arrays) - multilabel predictions
+#    debug (boolean) - whether to execute branching debug commands
+# OUTPUTS:
+#    nIncorrect (int) - number of false positive predictions
 def calcFalsePositive(labels,predictions,debug=False):
     labZeros = np.where(labels==0,1,0)
     correct = np.multiply(labZeros,predictions)
-    nCorrect = np.sum(correct,axis=0)
+    nIncorrect = np.sum(correct,axis=0)
     if(debug):
-        percCorrect = nCorrect/np.sum(labels,axis=0)*100
-        return([nCorrect,percCorrect])
-    return(nCorrect)
+        percCorrect = nIncorrect/np.sum(labels,axis=0)*100
+        return([nIncorrect,percCorrect])
+    return(nIncorrect)
 
+# calculate confusion matrix for a set of predictions 
+# INPUTS:
+#    labels (list of np int arrays) - multilabel classifications
+#    predictions (list of np int arrays) - multilabel predictions
+# OUTPUTS:
+#    confusion matrix as a pandas dataframe
 def calcConfusionMatrix(labels,predictions):
     predictionsRound = tf.round(tf.nn.sigmoid(predictions))
     confusionMatrix = ps.DataFrame({
@@ -92,7 +135,22 @@ def calcConfusionMatrix(labels,predictions):
     })
     return(confusionMatrix)
 
-predModel = tf.keras.models.load_model(secrets['TEXT_NO_VECTOR_MODEL_FILEPATH'],custom_objects={"TFBertModel": transformers.TFBertModel})
-predModel.compile(loss=[tf.keras.losses.BinaryCrossentropy()],optimizer=tf.keras.optimizers.Adam(learning_rate=0.0001),metrics=['accuracy'])
-labels,predictions = getPredictions(testData,predModel)
-calcConfusionMatrix(labels,predictions)
+if __name__ == "__main__":
+    
+    # test if gpus are available. If GPUs aren't available, the validation dataset can still be evaluated 
+    # but generating predictions may take several minutes
+    print("Num GPUs Available: ", len(tf.config.list_physical_devices('GPU')))
+    print(tf.config.list_physical_devices('GPU'))
+    
+    # load validation datset 
+    testData = loadDataset(secrets['TEST_DATA_FOLDER'])
+
+    # load deep learning model
+    predModel = tf.keras.models.load_model(secrets['TEXT_NO_VECTOR_MODEL_FILEPATH'],custom_objects={"TFBertModel": transformers.TFBertModel})
+    predModel.compile(loss=[tf.keras.losses.BinaryCrossentropy()],optimizer=tf.keras.optimizers.Adam(learning_rate=0.0001),metrics=['accuracy'])
+
+    # generate predictions from validation dataset
+    labels,predictions = getPredictions(testData,predModel)
+
+    # calculate confusion matrix for validation dataset
+    calcConfusionMatrix(labels,predictions)
--- deep_learning/analysis/predictAgesFromFace.py
+++ deep_learning/analysis/predictAgesFromFace.py
@@ -1,52 +1,55 @@
+### predictAgesFromFace.py ###
+### Author: Andrew Larkin ###
+
+### Summary: predict ages of children in social media imagery
+
 # Import dependencies
 import pandas as ps
 import numpy as np
-import matplotlib.pyplot as plt
-import cv2
 import os
-from zipfile import ZipFile
-import time
-from datetime import datetime
-import itertools
-from sklearn.model_selection import train_test_split
-from sklearn.metrics import confusion_matrix
 import tensorflow as tf
-from tensorflow.keras.models import Sequential
-from tensorflow.keras.layers import Dense
-from tensorflow.keras.layers import Conv2D, AveragePooling2D, GlobalAveragePooling2D, Dropout
-from tensorflow.keras import utils 
-from tensorflow.keras.callbacks import TensorBoard, ModelCheckpoint
 import glob
 
-# Setting random seeds to reduce the amount of randomness in the neural net weights and results
-# The results may still not be exactly reproducible
-np.random.seed(42)
-tf.random.set_seed(42)
+# load secrets
+from mySecrets import secrets
 
-PARENT_FOLDER = "/mnt/z/Aspire/Analyses/child/images/"
-IMAGE_FOLDER = PARENT_FOLDER + "faceClip/"
-PRED_FOLDER = PARENT_FOLDER + "predictions/"
+# custom loss function used to train age image deep learning model
+class CustomAccuracy(tf.keras.losses.Loss):
+        def __init__(self):
+            super().__init__()
+        def call(self, y_true, y_pred):
+
+            # predict if there is any child in the image
+            y_pred2 = tf.argmax(y_pred)
+            # look at labels to determine is there is any child in the image
+            y_true2 = tf.argmax(y_true)
+            diff = tf.math.abs(tf.math.subtract(y_pred2,y_true2))
+
+            # if the model or label are either positive for there being a child in the image,
+            # set the potential loss as 1.5 instead of 1 to make this mislcassification more 
+            # heavily weighted
+            maxLoss = tf.where(tf.math.equal(y_pred2,0),1.5,0)
+            maxLoss2 = tf.where(tf.math.equal(y_true2,0),1.5,0)          
+            maxLoss = tf.math.maximum(maxLoss,maxLoss2)
+
+            # calcualte the loss for mispredicting if a child is present in the image
+            loss1 = tf.cast(tf.math.reduce_mean(tf.math.minimum(tf.cast(diff,tf.float32),maxLoss),axis=None),tf.float32)
+
+            # calcualte the losses for each age group classification
+            cce = tf.keras.losses.CategoricalCrossentropy()
+            loss2 = cce(y_true,y_pred)
+
+            # return the sum of 2 losses
+            return (loss2+loss1)
 
 
-# Testing to ensure GPU is being utilized
-# Ensure that the Runtime Type for this notebook is set to GPU
-# If a GPU device is not found, change the runtime type under:
-# Runtime>> Change runtime type>> Hardware accelerator>> GPU
-# and run the notebook from the beginning again.
-
-device_name = tf.test.gpu_device_name()
-if device_name != '/device:GPU:0':
-    raise SystemError('GPU device not found')
-print('Found GPU at: {}'.format(device_name))
-
-
+# load image and convert to greyscale
 def _parse_function(filename):   
     image_string = tf.io.read_file(filename)
     image_decoded = tf.io.decode_jpeg(image_string, channels=1)    # channels=1 to convert to grayscale, channels=3 to convert to RGB.
-    # image_resized = tf.image.resize(image_decoded, [200, 200])
     return image_decoded
 
-
+# 
 def getImageList(inFolder):
     filepaths = glob.glob(inFolder + "*.jpg")
     print("found %i image files to read" %(len(filepaths)))
@@ -74,65 +77,45 @@ def getMaxProbs(seqId):
         probs = probs[0]
     newArr = np.concatenate(([nRows*1.0],probs),axis=0)
     return(newArr)
-     
 
-class CustomAccuracy(tf.keras.losses.Loss):
-        def __init__(self):
-            super().__init__()
-        def call(self, y_true, y_pred):
-            y_pred2 = tf.argmax(y_pred)
-            y_true2 = tf.argmax(y_true)
-            diff = tf.math.abs(tf.math.subtract(y_pred2,y_true2))
-            #diff = tf.math.multiply(diff,2)
-            maxLoss = tf.where(tf.math.equal(y_pred2,0),1.5,0)
-            maxLoss2 = tf.where(tf.math.equal(y_true2,0),1.5,0)
-            
-            maxLoss = tf.math.maximum(maxLoss,maxLoss2)
-            loss1 = tf.cast(tf.math.reduce_mean(tf.math.minimum(tf.cast(diff,tf.float32),maxLoss),axis=None),tf.float32)
-            cce = tf.keras.losses.CategoricalCrossentropy()
-            loss2 = cce(y_true,y_pred)
-            return (loss2+loss1)
+if __name__ == "__main__":
+
+    # Testing to ensure GPU is being utilized. If GPU is not available
+    # then generating predictions will take several minutes
+    device_name = tf.test.gpu_device_name()
+    if device_name != '/device:GPU:0':
+        raise SystemError('GPU device not found')
+    print('Found GPU at: {}'.format(device_name))
+
+    imgList = getImageList(secrets['IMAGE_FOLDER'])
+    predDataTensor = tf.constant(imgList)
+    predDataset = tf.data.Dataset.from_tensor_slices((predDataTensor))
+    predDataset = predDataset.map(_parse_function)
+    predDataset = predDataset.batch(512)
 
 
+    modelFile = secrets['MODEL_FILEPATH']
+    final_cnn = tf.keras.models.load_model(modelFile,compile=False)
+    final_cnn.compile(loss=[CustomAccuracy()],optimizer=tf.keras.optimizers.Adam(learning_rate=0.0001),metrics=['accuracy'])
+    final_cnn_pred = final_cnn.predict(predDataset)
+    imgDF = ps.DataFrame({
+        'imgName':imgList,
+    })
+    imgDF.to_csv(secrets['PER_FACE_CLASSIFICATIONS'],index=False)
+    np.save(secrets['PER_FACE_PROBABILITIES'], final_cnn_pred)
 
-imgList = getImageList(IMAGE_FOLDER)
-predDataTensor = tf.constant(imgList)
-predDataset = tf.data.Dataset.from_tensor_slices((predDataTensor))
-predDataset = predDataset.map(_parse_function)
-predDataset = predDataset.batch(512)
-
-
-modelFile = '/mnt/h/Aspire/faces/age_model_checkpoint_hybrid.h5'
-final_cnn = tf.keras.models.load_model(modelFile,custom_objects={'CustomAccuracy':CustomAccuracy},compile=False)
-final_cnn.compile(loss=[CustomAccuracy()],optimizer=tf.keras.optimizers.Adam(learning_rate=0.0001),metrics=['accuracy'])
-final_cnn_pred = final_cnn.predict(predDataset)
-print(final_cnn_pred[0:10])
-final_cnn_pred2 = final_cnn_pred.argmax(axis=-1)
-print(final_cnn_pred2[0:10])
-print(imgList[0:10])
-imgDF = ps.DataFrame({
-     'imgName':imgList,
-})
-imgDF.to_csv('/mnt/h/Aspire/faces/perFacePred.csv',index=False)
-np.save('/mnt/h/Aspire/faces/perFaceProbs.npy', final_cnn_pred)
-
-#imgDF = ps.read_csv('/mnt/h/Aspire/faces/perFacePred.csv')
-
-#final_cnn_pred = np.load('/mnt/h/Aspire/faces/perFaceProbs.npy')
+    imgDF['rowIndex'] = range(0, imgDF.count()[0])
+    imgDF['maxPred'] = final_cnn_pred.argmax(axis=-1)
+    imgDF = screenArgMax(imgDF)
 
 
-imgDF['rowIndex'] = range(0, imgDF.count()[0])
-imgDF['maxPred'] = final_cnn_pred.argmax(axis=-1)
-imgDF = screenArgMax(imgDF)
-
-
-imgList = list(imgDF['imgName'])
-idSeq = list(map(getIdFromFilepath,imgList))
-imgDF['idSeq'] = idSeq
-uniqueIds = list(set(idSeq))
-maxProbs = list(map(getMaxProbs,uniqueIds))
-df = ps.DataFrame({
-     'imgId':uniqueIds
-})
-df.to_csv('/mnt/h/Aspire/faces/perImgPred.csv',index=False)
-np.save('/mnt/h/Aspire/faces/perImgProbs.npy',maxProbs)
+    imgList = list(imgDF['imgName'])
+    idSeq = list(map(getIdFromFilepath,imgList))
+    imgDF['idSeq'] = idSeq
+    uniqueIds = list(set(idSeq))
+    maxProbs = list(map(getMaxProbs,uniqueIds))
+    df = ps.DataFrame({
+        'imgId':uniqueIds
+    })
+    df.to_csv(secrets['PER_IMAGE_CLASSIFICATIONS'],index=False)
+    np.save(secrets['PER_IMAGE_PROBABILITIES'],maxProbs)
