6d463d846f3dd817b481f8389c9895c1749f8d6e
Andrew Larkin <larkinan@oregonstate.edu>
2024-12-03 12:45:55 -0800

    cleaned up predictPlaceImage.py

--- deep_learning/implementation/predictPlaceImage.py
+++ deep_learning/implementation/predictPlaceImage.py
@@ -1,39 +1,37 @@
+# predictPlaceImge.py
+# Author: Andrew Larkin
+
+# Summary: predict place labels from social media imagery
+
 # Import dependencies
 import pandas as pd
 import numpy as np
-import matplotlib.pyplot as plt
-import cv2
 import os
-from zipfile import ZipFile
-import time
-from datetime import datetime
-import itertools
 import tensorflow as tf
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Dense
-from tensorflow.keras.layers import Conv2D, AveragePooling2D, GlobalAveragePooling2D, Dropout
-from tensorflow.keras import utils 
-from tensorflow.keras.callbacks import TensorBoard, ModelCheckpoint
+from tensorflow.keras.callbacks import ModelCheckpoint
 import tensorflow_hub as hub
 from mySecrets import secrets
-# Setting random seeds to reduce the amount of randomness in the neural net weights and results
-# The results may still not be exactly reproducible
-np.random.seed(42)
-tf.random.set_seed(42)
 
-# Testing to ensure GPU is being utilized
-# Ensure that the Runtime Type for this notebook is set to GPU
-# If a GPU device is not found, change the runtime type under:
-# Runtime>> Change runtime type>> Hardware accelerator>> GPU
-# and run the notebook from the beginning again.
 
-device_name = tf.test.gpu_device_name()
-if device_name != '/device:GPU:0':
-    raise SystemError('GPU device not found')
-print('Found GPU at: {}'.format(device_name))
+# load image from file and convert to grayscale
+# INPUTS:
+#    filename (str) - absolute filepath to image
+# OUTPUTS:
+#    image_decoded (tensor) - image values stored as a tf tensor
+def _parse_function(filename):   
+    image_string = tf.io.read_file(filename)
+    image_decoded = tf.io.decode_jpeg(image_string, channels=3)    # channels=1 to convert to grayscale, channels=3 to convert to RGB.
+    image_decoded = tf.cast(image_decoded, dtype=tf.float32)
+    
+    return(image_decoded)
 
-BATCH_SIZE = 64
-
+# convert string sequence of 0s and 1s to a binary numpy array
+# INPUTS:
+#    code (str) - string sequence of 0s and 1s
+# OUTPUTS:
+#    encoding (np integer array) - numpy array of 0s and 1s
 def convertCodeToNumpy(code):
     encoding = []
     for charIndex in range(len(code)):
@@ -44,51 +42,39 @@ def convertCodeToNumpy(code):
             encoding.append(1)
     return(encoding)
 
-# Defining a function to read the image, decode the image from given tensor and one-hot encode the image label class.
-# Changing the channels para in tf.io.decode_jpeg from 3 to 1 changes the output images from RGB coloured to grayscale.
-num_classes = 7
-def _parse_function(filename):   
-    image_string = tf.io.read_file(filename)
-    image_decoded = tf.io.decode_jpeg(image_string, channels=3)    # channels=1 to convert to grayscale, channels=3 to convert to RGB.
-    #image_decoded = tf.image.resize(image_decoded, [224, 224])
-    image_decoded = tf.cast(image_decoded, dtype=tf.float32)
-    
-    return(image_decoded)
-
-model = tf.keras.Sequential([
-    hub.KerasLayer("https://tfhub.dev/google/imagenet/resnet_v2_101/feature_vector/5",
-               trainable=False, arguments=dict(batch_norm_momentum=0.997)),
-    tf.keras.layers.Dense(100,activation='relu'),
-    tf.keras.layers.Dense(7,activation='sigmoid')
-])
-model.compile(
-    loss=[tf.keras.losses.BinaryCrossentropy(from_logits=True)],
-    optimizer=tf.keras.optimizers.Adam(learning_rate=0.0001),metrics=['accuracy'])#,run_eagerly=True)
-weightFile = secrets['PLACE_IMAGE_MODEL_WEIGHTS']
-checkpoint = ModelCheckpoint(filepath=weightFile,
-                                monitor='val_loss',
-                                save_best_only=True,
-                                save_weights_only=True,
-                                verbose=1,
-                                initial_value_threshold=  None#0.97828
-                                )
-model(np.zeros((1,224,224,3)))
-if(os.path.exists(weightFile)):
-    model.load_weights(weightFile)
-
-
+# given a filename, determine the original filename before name was appended with metadata
+# INPUTS:
+#    filename (str) - absolute filepath
+# OUTPUTS:
+#    root filename (str)
 def mapOrigFilepath(filename):
     return filename[:-11] + ".jpg"
 
+# load images from disk to numpy array
+# INPUTS:
+#    imgFilepaths (str) - absolute filepaths to imagery
+# OUTPUTS:
+#    arr (numpy matrix) - contains set of 2d imagery in np arrays
 def loadImgs(imgFilepaths):
     arr = list(map(_parse_function,imgFilepaths))
     arr = np.asarray(arr)
     return arr
 
+# given folderpath, create list of absolute filepaths for files in the folder
+# INPUTS:
+#    foldername (str) - folder name containing files to process. Not folderpath!
+# OUTPUTS:
+#    filesToProcess (str list) - absolute filepaths to files to process
+#    origFilenames (str list) - original filenames before names were appended with metadata
 def prepFilepaths(foldername):
     
     folderToProcess = secrets['PLACE_IMAGE_INPUT_FOLDER'] + foldername
     
+    # given relative filepath, return absolute filepath
+    # INPUTS:
+    #    filename (str) - relative filepath
+    # OUTPUTS:
+    #    absolute filepath (str)
     def mapFullFilepath(filename):
         return folderToProcess + '/' + filename
     
@@ -97,12 +83,20 @@ def prepFilepaths(foldername):
     origFilenames = list(map(mapOrigFilepath,filenames))
     return([filesToProcess,origFilenames])
 
+# create csv containing filenames and model predictions 
+# INPUTS:
+#    foldername (str) - folder that was processed. Includes metadata including year and category
+#    predictions (np matrix) - model predictions for each label
+#    origFilenames (str array) - image names that correspond to model predictions
 def createCSV(foldername,predictions,origFilenames):
     df = pd.DataFrame(data = predictions,  
                   columns = ['isDaycare','isPark','isHome','isSchool','isNeighborhood','isIndoor','isOutdoor'])
     df['filename'] = origFilenames
     df.to_csv(secrets['PLACE_IMAGE_PRED_FOLDER'] + foldername + ".csv",index=False)
 
+# generate predictions for all images within a folder
+# INPUTS:
+#    foldername (str) - name of folder containing images. Does not include folderpath!
 def processSingleFolder(foldername):
     outputFilename = secrets['PLACE_IMAGE_PRED_FOLDER'] + foldername + '.csv'
     if (os.path.exists(outputFilename)):
@@ -113,6 +107,43 @@ def processSingleFolder(foldername):
     preds = model.predict(imgs)
     createCSV(foldername,preds,origFilenames)
 
-foldersToProcess = os.listdir(secrets['PLACE_IMAGE_INPUT_FOLDER'])
-for folder in foldersToProcess:
-    processSingleFolder(str(folder))
+# main function
+if __name__ == '__main__':
+
+    # Testing to ensure GPU is being utilized
+    device_name = tf.test.gpu_device_name()
+    if device_name != '/device:GPU:0':
+        raise SystemError('GPU device not found')
+    print('Found GPU at: {}'.format(device_name))
+
+   # number of labels in the place model
+    num_classes = 7
+    # number of images per batch
+    BATCH_SIZE = 64
+
+    # create model, compile, and load weights
+    model = tf.keras.Sequential([
+        hub.KerasLayer("https://tfhub.dev/google/imagenet/resnet_v2_101/feature_vector/5",
+                trainable=False, arguments=dict(batch_norm_momentum=0.997)),
+        tf.keras.layers.Dense(100,activation='relu'),
+        tf.keras.layers.Dense(7,activation='sigmoid')
+    ])
+    model.compile(
+        loss=[tf.keras.losses.BinaryCrossentropy(from_logits=True)],
+        optimizer=tf.keras.optimizers.Adam(learning_rate=0.0001),metrics=['accuracy'])#,run_eagerly=True)
+    weightFile = secrets['PLACE_IMAGE_MODEL_WEIGHTS']
+    checkpoint = ModelCheckpoint(filepath=weightFile,
+                                    monitor='val_loss',
+                                    save_best_only=True,
+                                    save_weights_only=True,
+                                    verbose=1,
+                                    initial_value_threshold=  None#0.97828
+                                    )
+    model(np.zeros((1,224,224,3)))
+    if(os.path.exists(weightFile)):
+        model.load_weights(weightFile)
+
+    # get list of folders to process. Predict labels for one folder at a time
+    foldersToProcess = os.listdir(secrets['PLACE_IMAGE_INPUT_FOLDER'])
+    for folder in foldersToProcess:
+        processSingleFolder(str(folder))
