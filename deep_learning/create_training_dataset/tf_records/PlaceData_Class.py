0b9ac1fef87c1758c01a5ce9089e72a993887c31
Andrew Larkin <larkinan@oregonstate.edu>
2024-12-03 09:32:58 -0800

    added comments to PlaceData_Class.py

--- deep_learning/create_training_dataset/tf_records/PlaceData_Class.py
+++ deep_learning/create_training_dataset/tf_records/PlaceData_Class.py
@@ -1,13 +1,32 @@
+# PlacedData_Class.py
+# Author: Andrew Larkin
+
+# Summary: Custom Data class for cleaning and processing 'Place'-labeled social media posts into tf records 
+#          for training deep learning models
+
+# import libraries
 import numpy as np
 import tensorflow as tf
 from transformers import *
+
+# import custom classes
 from TFRecordWrite_Class import TFRecordWrite
 
 class PlaceData():
+    # create instance of place data class
+    # INPUTS:
+    #    outputFilepath (str) - absolute filepath where tf records will be stored
+    #    TokenPath (str) - absolute fiepath where custom BERT tokenizer is stored
     def __init__(self,outFilepath,TokenPath):  
+
+        # create TFRecordWrite object for writing tf records. #5000 is the test dataset size
         self.TFRecordWriter = TFRecordWrite(5000)
         self.outFilepath = outFilepath
+
+        # load BERT tokenizer from disk
         self.tokenizer = BertTokenizer.from_pretrained(TokenPath)
+
+        # kv pairs for converting place type labels into binary array
         self.placeCodingDict = {
             'Childcare/daycare':0,
             'Park/playground/child sports center':1,
@@ -18,28 +37,53 @@ class PlaceData():
             'Unsure':5,
             'No location':6
         }
+
+        # kv pairs for converting indoor/outdoor labels into binary array
         self.inDoorOutdoor = {
             'Indoor location':7,
             'Outdoor location':8,
             'No location/unsure':9
         }
 
-    def convertPlaceCodeToLabels(self,record):
+    # given place labels in text format, convert to binary array labels 
+    # INPUTS:
+    #    placeCode (str) - place labels in text format
+    # OUTPUTS:
+    #    place labels in binary np array format
+    def convertPlaceCodeToLabels(self,placeCode):
+
+        # get possible text labels
         keys = list(self.placeCodingDict.keys())
         keys2 = list(self.inDoorOutdoor.keys())
+
+        # initialize output array with 0s. 
         codeArr = [0 for x in range(10)]
+
+        # for each possible text label, test is label is in label assigned by worker 
+        # and if so change the corresponding digit in the output array to 1
         for key in keys:
-            if key in record['location']:
+            if key in placeCode['location']:
                 codeArr[self.placeCodingDict[key]] = 1
         for key in keys2:
-            if key in record['location_cat']:
+            if key in placeCode['location_cat']:
                 codeArr[self.inDoorOutdoor[key]] = 1
         return(np.asarray(codeArr))
  
+    # replace new line characters with a period and space
+    # INPUTS:
+    #    origLine (str) - line to replace characters in
+    # OUTPUTS:
+    #    newLine (str) - line with new line characters replaced with ". "
     def mapNewLineReplace(self,origLine):
         newLine = origLine.replace("\n",". ")
         return(newLine)
     
+    # convert statistics of the author who posted the social media post to 
+    # a np array
+    # INPUTS:
+    #    userDF (pandas dataframe) - statistics of the social media post author
+    # OUTPUTS:
+    #    np array, with percentages of each health label in a seperate inde
     def convertUserStatsToArray(self,userDF):
         codeArr = [0.0 for x in range(7)]
         nPosted = userDF['nPosted']
@@ -52,6 +96,12 @@ class PlaceData():
         codeArr[6] = userDF['nOutdoor']/nPosted
         return(np.asarray(codeArr))
         
+    # given social media post labels and metadata, transform the data into arrays for tf records
+    # INPUTS:
+    #    tweetData (pandas dataframe) - contains social media post text, labels, and metadata
+    #    vectorData (np array) - document vectors for social media authors
+    # OUTPUTS:
+    #    pandas array, with input records preprocessed for saving as tf records
     def setupModelInputs(self,tweetData,vectorData):
         tweetText = list(tweetData['text'])
         tweetText = list(map(self.mapNewLineReplace,tweetText))
@@ -65,7 +115,13 @@ class PlaceData():
         inputs['user_stats'] = userStats
         return(inputs)
 
+    # create tf records from social media and author data
+    # INPUTS:
+    #    tweetArray (pandas dataframe) - social media labels and metadata
+    #    vectorArray (np array) - document vectors for social media authors
     def convertTextFileToTFRecord(self,tweetArray,vectorArray):
         modelInputs = self.setupModelInputs(tweetArray,vectorArray)
         print("completed creating model inputs")
         self.TFRecordWriter.write_tweet_text_to_tfr_short(modelInputs,self.outFilepath,'place')
+
+# end of PlaceData_Class.py
