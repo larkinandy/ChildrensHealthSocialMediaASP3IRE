ed95ad05e8e6247cd53761c8137e86c603f73146
Andrew Larkin <larkinan@oregonstate.edu>
2024-12-04 16:21:23 -0800

    cleaned up training scripts
    cleaned up trainMultiModel.py
    cleaned up trainTextModel.py

--- deep_learning/train_models/multiModalModels/trainMultiModel.py
+++ deep_learning/train_models/multiModalModels/trainMultiModel.py
@@ -1,3 +1,9 @@
+# trainMultiModel.py
+# Author: Andrew Larkin
+
+# Summary: train a model that uses both text and imagery to predict child age groups
+
+# import libraries
 import pandas as ps
 import tensorflow as tf
 import os
@@ -6,8 +12,15 @@ import numpy as np
 from transformers import BertTokenizer, TFBertForMaskedLM
 from keras.callbacks import ModelCheckpoint
 from transformers import *
+
+# import secrets
 from mySecrets import secrets
 
+# create multi class encoding label
+# INPUTS:
+#    curRecord (pandas dataframe) - contains one record (row)
+# OUTPUTS:
+#    np binary array
 def createLabel(curRecord):
     return([
         curRecord['isBaby'],
@@ -18,8 +31,11 @@ def createLabel(curRecord):
         curRecord['isChild']
     ])
 
+# define model architecture
+# OUTPUTS:
+#    model (keras model) - contains model architecture
 def createHybridModel():
-    inputVals = tf.keras.layers.Input((14), dtype=tf.float32, name='inputs')
+    inputVals = tf.keras.layers.Input((14), dtype=tf.float32, name='inputs') # 7 inputs from text model, 7 from image model
     hiddenLayer = tf.keras.layers.Dense(1024,activation='relu',name='hidden')(inputVals)
     hiddenLayer = tf.keras.layers.Dropout(0.2)(hiddenLayer)
     hiddenLayer = tf.keras.layers.Dense(128,activation='relu')(hiddenLayer)
@@ -27,15 +43,31 @@ def createHybridModel():
     model = tf.keras.Model(inputs = inputVals,outputs = predLayer)
     return(model)
 
+# combine text and image model predictions to create a multimodal input dataset
+# INPUTS:
+#    textFilepath (str) - absolute filepath to social media posts with text
+#    textModelPreds (str) - absolute filepath to text model predictions
+#    imageModelPreds (str) - absolute filepath to image model predictions
+#    recordsWithImgs (pandas dataframe) - social media posts with imagery
+# OUTPUTS:
+#    testInputs (np matrix) - inputs for the multimodal model
+#    testLabels (np matrix) - labels for the multimodal model
 def loadDataset(textFilepath,textModelPreds,imageModelPreds,recordsWithImgs):
+
+    # load text social meida posts and inner join with those that also have imagery
     testText = ps.read_csv(textFilepath,encoding='utf-8',lineterminator='\n')
     testText['imgKey'] = testText['img_http'].apply(lambda x: x[:-4])
     testText['textIndex'] = range(testText.count().iloc[0])
     joined = testText.merge(recordsWithImgs,how='inner',on='imgKey')
     joined.drop_duplicates(inplace=True)
+
+    # load text and image model predictions
     faceProbs = np.load(imageModelPreds)
     textProbs = np.load(textModelPreds)
     testHybridRecords,testLabels = [],[]
+
+    # combine text and image model predictions to create single input into 
+    # multimodal input
     for recordNum in range(joined.count().iloc[0]):
         curRecord = joined.iloc[recordNum]
         curImgIndex = curRecord['imgIndex']
@@ -49,35 +81,54 @@ def loadDataset(textFilepath,textModelPreds,imageModelPreds,recordsWithImgs):
     testLabels = np.stack(testLabels)
     return(testInputs,testLabels)
 
-def runModel(model,trainHybridInputs,trainHybridLabels,testHybridInputs,testHybridLabels):
+# train a multimodal model 
+# INPUTS:
+#    model (keras model) - contains model architecture
+#    trainHybridInputs (np matrix) - inputs to train model
+#    trainHybridLabels (np matrix) - labels to train model
+#    testHybridInputs (np matrix) - inputs to test model
+#    testHybridLabels (np matrix) - labels to test model
+def trainModel(model,trainHybridInputs,trainHybridLabels,testHybridInputs,testHybridLabels):
     
+    # compile model
     model.compile(loss=[tf.keras.losses.BinaryCrossentropy(from_logits=True)],optimizer=tf.keras.optimizers.Adam(learning_rate=0.0001))
     
-    checkpoint = ModelCheckpoint(filepath=secrets['HYBRID_MODEL_CHECKPOINT'],
-                                monitor='val_loss',
-                                save_best_only=True,
-                                save_weights_only=False,
-                                verbose=0,
-                                initial_value_threshold=  None
+    # define conditions for saving model weights
+    checkpoint = ModelCheckpoint(
+        filepath=secrets['HYBRID_MODEL_CHECKPOINT'],
+        monitor='val_loss',
+        save_best_only=True,
+        save_weights_only=False,
+        verbose=0,
+        initial_value_threshold=  None
     )
                                 
-    model.fit(trainHybridInputs,trainHybridLabels,
-                            batch_size=512,
-                            validation_data=(testHybridInputs,testHybridLabels),
-                            epochs=5000,
-                            callbacks=[checkpoint],
-                            shuffle=True   
+    # train model
+    model.fit(
+        trainHybridInputs,
+        trainHybridLabels,
+        batch_size=512,
+        validation_data=(testHybridInputs,testHybridLabels),
+        epochs=5000,
+        callbacks=[checkpoint],
+        shuffle=True   
     )
 
-recordsWithImgs = ps.read_csv(secrets['RECORDS_WITH_IMGS_FILEPATH'])
-recordsWithImgs['imgKey'] = recordsWithImgs['imgId']
-recordsWithImgs['imgIndex'] = range(recordsWithImgs.count()[0])
-recordsWithImgs = recordsWithImgs[['imgKey','imgIndex']]
+# main function
+if __name__ == '__main__':
 
-testInputs,trainlabels = loadDataset(secrets['TEST_TEXT_FILEPATH'],secrets['TEST_TEXT_PREDICTIONS'],secrets['TEST_IMAGE_PREDICTIONS'],recordsWithImgs)
-trainInputs,trainLabels = loadDataset(secrets['TRAIN_TEXT_FILEPATH'],secrets['TRAIN_TEXT_PREDICTIONS'],secrets['TRAIN_IMAGE_PREDICTIONS'],recordsWithImgs)
+    # load train and test datasets
+    recordsWithImgs = ps.read_csv(secrets['RECORDS_WITH_IMGS_FILEPATH'])
+    recordsWithImgs['imgKey'] = recordsWithImgs['imgId']
+    recordsWithImgs['imgIndex'] = range(recordsWithImgs.count()[0])
+    recordsWithImgs = recordsWithImgs[['imgKey','imgIndex']]
 
-with tf.device('/GPU:0'):
-    hybridModel = createHybridModel()
-    runModel(hybridModel)
+    testInputs,trainlabels = loadDataset(secrets['TEST_TEXT_FILEPATH'],secrets['TEST_TEXT_PREDICTIONS'],secrets['TEST_IMAGE_PREDICTIONS'],recordsWithImgs)
+    trainInputs,trainLabels = loadDataset(secrets['TRAIN_TEXT_FILEPATH'],secrets['TRAIN_TEXT_PREDICTIONS'],secrets['TRAIN_IMAGE_PREDICTIONS'],recordsWithImgs)
 
+    # train model
+    with tf.device('/GPU:0'):
+        hybridModel = createHybridModel()
+        trainModel(hybridModel)
+
+# end of trainMultiModel.py
--- deep_learning/train_models/textModels/trainTextModel.py
+++ deep_learning/train_models/textModels/trainTextModel.py
@@ -1,31 +1,27 @@
+# trainTextModel.py
+# Author: Andrew Larkin
+
+# Summary: train a model to predict age,place,or health classifications from social media text
+
+# import libraries
 import tensorflow as tf
 from transformers import BertTokenizer, TFBertForMaskedLM
 from keras.callbacks import ModelCheckpoint
 from transformers import *
 import keras
+import sys
+
+# import custom classes and secrets
 from TFRecordRead_Class import TFRecordRead
 from mySecrets import secrets
     
 
-print("Num GPUs Available: ", len(tf.config.list_physical_devices('GPU')))
-print(tf.config.list_physical_devices('GPU'))
-AUTOTUNE = tf.data.experimental.AUTOTUNE
-BATCH_SIZE = 128
-MAX_SEQ_LEN = 100
-VECTOR_SIZE = 512
-USER_STATS_SIZE = 6
-
-# load runtime arguments
-USE_VECTOR = sys.argv[1] # Boolean, whether to use user vector inputs
-USE_STATS = sys.argv[2] # Boolean, whether to use user stats inputs
-modelType = sys.argv[3] # string, either 'age','place', or 'health'
-
-if(modelType=='age'):
-    PARENT_FOLDER = secrets['CHILD_FOLDER']
-elif(modelType=='place'):
-    PARENT_FOLDER = secrets['PLACE_FOLDER']
-else:
-    PARENT_FOLDER = secrets['HEALTH_FOLDER']
+# load tf records into memory
+#    dataFolder (str) - absolute folderpath where tf records are stored
+#    vector (Boolean) - whether to include user summary vectors in model
+#    userStats (Boolean) - whether to include user statistics in model
+# OUTPUTS:
+#    TFRecordRead object for serving records to a tf model
 def loadDataset(dataFolder,vector=False,userStats=False):
     recordReader = TFRecordRead(dataFolder,BATCH_SIZE,vector,userStats)
     dataset = recordReader.tfRecords.prefetch(buffer_size=AUTOTUNE)
@@ -33,25 +29,18 @@ def loadDataset(dataFolder,vector=False,userStats=False):
     print("loaded dataset")
     return(dataset)
 
-
-def build_base():
-    t_i = tf.keras.layers.Input((MAX_SEQ_LEN), dtype=tf.int32, name='input_ids')
-    m_i = tf.keras.layers.Input((MAX_SEQ_LEN,), dtype=tf.int32, name='atttention_mask')
-    model = TFBertForMaskedLM.from_pretrained(secrets['EXPANDED_BERT_BASE_FILEPATH'])(input_ids=t_i,attention_mask=m_i)
-    model = tf.keras.Model([t_i,m_i],model)
-    return model 
-
-def saveBERTWeights(model):
-    new_model = model.layers[1:][1]
-    new_model.save_pretrained(secrets['PRETRAINED_BERT_BASE_FILEPATH'])
-
-def loadBertWeights(baseModel):
-    saveBERTWeights(baseModel)
-
+# build classification model architecture 
+# INPUTS:
+#    vector (Boolean) - whether to include user summary vectors in model architecture
+#    userStats (Boolean) - whether to include user statistics in model architecture
+#    fixWeights (Boolean) - whether to fix the BERT layer weights during trianing
+#    nOutputs (int) - number of multiclasses to predict
+# OUTPUTS:
+#    bm2 (tf keras model) - model arcchitecture
 def buildClassifictionModel(vector=False,userStats=False,fixWeights=False,nOutputs=6):
-    MAX_SEQ_LEN = 100
-    #baseModel = build_base()
-    #baseModel.load_weights(weightFile)
+    MAX_SEQ_LEN = 100 # max number of words for a sentence 
+
+    # define inputs 
     t_i = tf.keras.layers.Input((MAX_SEQ_LEN), dtype=tf.int32, name='input_ids')
     m_i = tf.keras.layers.Input((MAX_SEQ_LEN,), dtype=tf.int32, name='atttention_mask')
     inVals = [t_i,m_i]
@@ -61,6 +50,8 @@ def buildClassifictionModel(vector=False,userStats=False,fixWeights=False,nOutpu
     if(userStats):
         u_i = tf.keras.layers.Input((USER_STATS_SIZE), dtype=tf.float32, name='user_stats')
         inVals.append(u_i)
+
+    # load BERT weights into first few layers of the model
     btok = TFBertModel.from_pretrained(secrets['PRETRAINED_BERT_BASE_FILEPATH'])
 
     #experimental block for freezing BERT weights. Did not improve model performance
@@ -79,15 +70,17 @@ def buildClassifictionModel(vector=False,userStats=False,fixWeights=False,nOutpu
                         break
                 else:
                     w._trainable=False
-
-
     bertOutput = btok([t_i,m_i])
     combinedOutput = (bertOutput['pooler_output'])
+
+    # add vector and user stats if input args were True
     if(vector):
         combinedOutput = tf.concat([combinedOutput,v_i],-1)
     if(userStats):
         combinedOutput = tf.concat([combinedOutput,u_i],-1)
     combinedOutput = tf.keras.layers.Dropout(0.2)(combinedOutput)
+
+    # fully connected layers before final output
     newLayers = tf.keras.layers.Dense(128,activation='relu',name='activate1')(combinedOutput)
     newLayers = tf.keras.layers.Dropout(0.2)(newLayers)
     newLayers = tf.keras.layers.Dense(128,activation='relu',name='activate2')(newLayers)
@@ -96,36 +89,71 @@ def buildClassifictionModel(vector=False,userStats=False,fixWeights=False,nOutpu
     bm2 = tf.keras.Model(inputs = [inVals],outputs = final)
     return(bm2)
 
-
-def runModel(model):
+# train the classification model
+# INPUTS:
+#    model (tf keras model) - contains model architecture
+def trainModel(model):
     model.compile(
         loss=[tf.keras.losses.BinaryCrossentropy(from_logits=True)],
         optimizer=tf.keras.optimizers.Adam(learning_rate=0.0001)
     )
     
-    checkpoint = ModelCheckpoint(filepath=PARENT_FOLDER + secrets['MODEL_CHECKPOINT_FILEPATH'],
-                                monitor='val_loss',
-                                save_best_only=True,
-                                save_weights_only=False,
-                                verbose=1,
-                                initial_value_threshold=  None
+    # define conditions for saving model weights
+    checkpoint = ModelCheckpoint(
+        filepath=PARENT_FOLDER + secrets['MODEL_CHECKPOINT_FILEPATH'],
+        monitor='val_loss',
+        save_best_only=True,
+        save_weights_only=False,
+        verbose=1,
+        initial_value_threshold=  None
     )
                                 
-    model.fit(trainData,
-                            batch_size=BATCH_SIZE,
-                            validation_data=testData,
-                            epochs=1000,
-                            callbacks=[checkpoint],
-                            shuffle=True    # shuffle=False to reduce randomness and increase reproducibility
+    # train model                                
+    model.fit(
+        trainData,
+        batch_size=BATCH_SIZE,
+        validation_data=testData,
+        epochs=1000,
+        callbacks=[checkpoint],
+        shuffle=True    # shuffle=False to reduce randomness and increase reproducibility
     )
 
+# main function
+if __name__ == '__main__':
 
-trainData = loadDataset(PARENT_FOLDER + secrets['TRAIN_DATA_SUBFOLDER'],USE_VECTOR,USE_STATS)
-testData = loadDataset(PARENT_FOLDER + secrets['TEST_DATA_SUBFOLDER'],USE_VECTOR,USE_STATS)
+    print("Num GPUs Available: ", len(tf.config.list_physical_devices('GPU')))
+    print(tf.config.list_physical_devices('GPU'))
+    AUTOTUNE = tf.data.experimental.AUTOTUNE
 
-with tf.device('/GPU:0'):
-    classificationModel = buildClassifictionModel(USE_VECTOR,USE_STATS)
-    
-    print(classificationModel.summary())
+    # define global constants
+    BATCH_SIZE = 128
+    MAX_SEQ_LEN = 100
+    VECTOR_SIZE = 512
+    USER_STATS_SIZE = 6
 
-    runModel(classificationModel)
+    # load runtime arguments
+    USE_VECTOR = sys.argv[1] # Boolean, whether to use user vector inputs
+    USE_STATS = sys.argv[2] # Boolean, whether to use user stats inputs
+    modelType = sys.argv[3] # string, either 'age','place', or 'health'
+
+    # set model type depending on which labels a model should be trained to predict
+    if(modelType=='age'):
+        PARENT_FOLDER = secrets['CHILD_FOLDER']
+    elif(modelType=='place'):
+        PARENT_FOLDER = secrets['PLACE_FOLDER']
+    else:
+        PARENT_FOLDER = secrets['HEALTH_FOLDER']
+
+    # load training and test datasets
+    trainData = loadDataset(PARENT_FOLDER + secrets['TRAIN_DATA_SUBFOLDER'],USE_VECTOR,USE_STATS)
+    testData = loadDataset(PARENT_FOLDER + secrets['TEST_DATA_SUBFOLDER'],USE_VECTOR,USE_STATS)
+
+    with tf.device('/GPU:0'):
+        # build model architecture
+        classificationModel = buildClassifictionModel(USE_VECTOR,USE_STATS)
+        print(classificationModel.summary())
+
+        # train the model
+        trainModel(classificationModel)
+
+# end of trainTextModel.py
