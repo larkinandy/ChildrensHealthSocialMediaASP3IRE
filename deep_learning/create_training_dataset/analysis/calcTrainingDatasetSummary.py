ac143fd92d06907f917c32ade7f5f2f503e6a37e
Andrew Larkin <larkinan@oregonstate.edu>
2024-12-09 17:05:39 -0800

    update gitignore

---
+++ deep_learning/create_training_dataset/analysis/calcTrainingDatasetSummary.py
@@ -0,0 +1,14 @@
+# calcTrainingDatasetSummary.py 
+# Author: Andrew Larkin
+
+# Summary: calculate number and percent positive tweets for each 
+#          keyword used to download tweets
+
+# import libraries
+import sys
+import os
+import pandas as ps
+
+# import secrets
+from mySecrets import secrets
+
